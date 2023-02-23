f9a084778a3e18751cd10a777b8fc0d20c0bc9c5
estevamponte-wmg <89148918+EstevamPonteOSF@users.noreply.github.com>
2023-02-23 09:54:16 -0300

    Day 3 exercise 5

--- app/pages/product-details/index.jsx
+++ app/pages/product-details/index.jsx
@@ -9,13 +9,35 @@ import React, {useEffect, useState} from 'react'
 import PropTypes from 'prop-types'
 import {Helmet} from 'react-helmet'
 import {FormattedMessage, useIntl} from 'react-intl'
-
-// Components
-import {
-    Text
-} from '@chakra-ui/react'
+import {Tooltip, Spinner, Text} from '@chakra-ui/react'
+import {useCommerceAPI} from '../../commerce-api/contexts'
 
 const ProductDetails = ({product}) => {
+    const api = useCommerceAPI()
+    const [promotionMap, setPromotionMap] = useState([])
+    const {productPromotions} = product
+    const handleHover = (id) => {
+        // Don't make a network request if you already loaded this data.
+
+       const alreadyIn = promotionMap.findIndex((promo) => promo.id === id) > -1
+        if (alreadyIn) {
+            return
+        }
+        const getPromotion = async (id) => {
+            const promotions = await api.shopperPromotions.getPromotions({
+                parameters: {ids: id}
+            })
+            const newObject ={
+                [id]: promotions.data[0]
+            }
+            console.log(newObject)
+            setPromotionMap((current) => {
+                return [...current, promotions.data[0]]
+            })
+        }
+        getPromotion(id)
+    }
+
     return (
         <div className="t-product-details" itemScope itemType='http://schema.org/Product'>
             <Text>This product is: {product.name}</Text>
@@ -25,6 +47,34 @@ const ProductDetails = ({product}) => {
                     <meta name='description' content={product.name}/>
                 </Helmet>
             )}
+
+            {/* <Text>These are thepromotions</Text>
+            {promotions && promotions.map(({id, calloutMsg, details}) => (
+                <Tooltip key={id} label={details} aria-label="Promotion detail">
+                    <Text >{calloutMsg}</Text>
+                </Tooltip>
+            ))} */}
+            <Text>These are the promotions (if any):</Text>
+            {productPromotions &&
+                productPromotions.map(({promotionId, calloutMsg}) => {
+                    const alreadyIn = promotionMap.findIndex((promo) => promo.id === promotionId) > -1
+                    let promo = promotionMap.find((promo) => promo.id === promotionId)
+                    
+                    return (
+                    <Tooltip
+                        onOpen={() => {
+                            handleHover(promotionId)
+                        }}
+                        key={promotionId}
+                        label={ (alreadyIn && promo.details) || (<Spinner />)
+                    }
+                    aria-label="Promotion details"
+                    >
+                    <Text>{calloutMsg}</Text>
+                    </Tooltip>
+                    )
+                })
+            }
         </div>
     )
 }
@@ -37,12 +87,19 @@ ProductDetails.shouldGetProps = async ({previousParams, params}) => {
 
 ProductDetails.getProps = async ({params, api}) => {
     await api.auth.login()
-    console.log(api)
     const product = await api.shopperProducts.getProduct({
         parameters: {id: params.productId, allImages: true}
     })
+
+    // const promotionsIds = plucksIds(product.productPromotions, "promotionId")
+
+    // const promotions = await api.shopperPromotions.getPromotions({
+    //     parameters: {ids: promotionsIds}
+    // })
+
     return {
-        product: product
+        product: product,
+        // promotions: promotions
     }
 }
 
@@ -50,6 +107,7 @@ ProductDetails.propType = {
     errorMessage: PropTypes.string,
     product: PropTypes.object,
     params: PropTypes.object,
+    // promotions: PropTypes.array,
     trackPageLoad: PropTypes.func
 }
 
